94c0729e7f628cc7bab5ad18ff0a97dacceef926
artemqdh <artjom-05@mail.ru>
2024-05-19 20:22:04 +0300

    new commit dz

--- data.h
+++ data.h
@@ -1,9 +1,36 @@
 #pragma once
+#include <stdexcept>
 
 namespace data
 {
     struct Data
     {
+        Data() = default;
+        Data(std::string datacopy)
+        {
+            std::string DD = datacopy.substr(0, 2);
+            std::string MM = datacopy.substr(3, 2);
+            std::string YY = datacopy.substr(6, 4);
+            this->day = atoi(DD.c_str());
+            this->month = atoi(MM.c_str());
+            this->year = atoi(YY.c_str());
+            //Place for your decision
+            //...
+            if (day > 31 || month > 12 || year < 2024)
+            {
+                throw std::domain_error("Error. Date is not normal.");
+            }
+        }
+
+        Data operator = (const Data& rhs)
+        {
+            day = rhs.day;
+            month = rhs.month;
+            year = rhs.year;
+
+            return *this;
+        }
+
         int day;
         int month;
         int year;
--- expenses.cpp
+++ expenses.cpp
@@ -41,6 +41,45 @@ namespace expenses
         return category;
     }
 
+    std::vector<Expenses*> Report_Expenses_Week(std::vector<Expenses>& ListofExpenses)
+    {
+        std::vector<Expenses*> all_expenses_by_week;
+        std::cout << "Type in the date of today: " << std::endl;
+        std::string datacopy;
+        std::cin >> datacopy;
+
+        data::Data date_ex(datacopy);
+
+        tm current_date = {};
+
+        current_date.tm_year = date_ex.year - 1900;
+        current_date.tm_mon = date_ex.month - 1;
+        current_date.tm_mday = date_ex.day;
+
+        for (int i = 0; i < 7; ++i)
+        {
+            std::time_t next_day = std::mktime(&current_date) + i * 24 * 60 * 60;
+            std::tm* next_date = std::localtime(&next_day);
+
+            std::cout << next_date->tm_mday << "." << next_date->tm_mon + 1 << "." << next_date->tm_year + 1900 << std::endl;
+
+            date_ex.day = next_date->tm_mday;
+            date_ex.month = next_date->tm_mon + 1;
+            date_ex.year = next_date->tm_year + 1900;
+
+            for (int a = 0; a < ListofExpenses.size(); ++a)
+            {
+                if (date_ex.day == ListofExpenses[a].getDate().day && date_ex.month == ListofExpenses[a].getDate().month && date_ex.year == ListofExpenses[a].getDate().year)
+                {
+
+                    all_expenses_by_week.push_back(&ListofExpenses[a]);
+                }
+            }
+
+        }
+        return all_expenses_by_week;
+    }
+
     void printTop3Expenses(std::vector<Expenses>& ListofExpenses)
     {
         std::sort(ListofExpenses.begin(), ListofExpenses.end(), [](const Expenses& exp1, const Expenses& exp2)
@@ -55,14 +94,15 @@ namespace expenses
         }
     }
 
-    void printTop3ExpensesByCategory(std::vector<Expenses>& ListofExpenses)
+
+    void printTop3ExpensesByCategory(std::vector<Expenses*> ListofExpenses)
     {
         std::vector<std::pair<std::string, int>> categoryExpenses;
 
         for (const auto& exp : ListofExpenses)
         {
-            std::string category = exp.getCategory();
-            int amount = exp.getAmount();
+            std::string category = exp->getCategory();
+            int amount = exp->getAmount();
 
             auto i = find_if(categoryExpenses.begin(), categoryExpenses.end(), [&category](const std::pair<std::string, int>& p)
                 {
@@ -91,29 +131,54 @@ namespace expenses
         }
     }
 
+
+    void printTop3ExpensesByCategory_week(std::vector<Expenses>& ListofExpenses)
+    {
+        std::vector<Expenses*> all_expenses_by_week= Report_Expenses_Week(ListofExpenses);
+        printTop3ExpensesByCategory(all_expenses_by_week);
+    }
+
+    void printTop3ExpensesByCategory_month(std::vector<Expenses>& ListofExpenses)
+    {
+           std::cout << "Type in the date of today: " << std::endl;
+            std::string datacopy;
+            std::cin >> datacopy;
+
+            data::Data date_ex(datacopy);
+
+            tm current_date = {};
+
+            current_date.tm_year = date_ex.year - 1900;
+            current_date.tm_mon = date_ex.month - 1;
+            current_date.tm_mday = date_ex.day;
+
+            std::vector<Expenses*> all_expenses_by_week;
+
+            for (int i = 0; i < ListofExpenses.size(); ++i)
+            {
+
+                if (date_ex.month == ListofExpenses[i].getDate().month && date_ex.year == ListofExpenses[i].getDate().year)
+                {
+                    all_expenses_by_week.push_back(&ListofExpenses[i]);
+                }
+            }
+            printTop3ExpensesByCategory(all_expenses_by_week);
+    }
+
+
     void PrintAll(std::vector<Expenses>& ListofExpenses)
     {
         std::cout << "Type in the date of today: " << std::endl;
         std::string datacopy;
         std::cin >> datacopy;
 
-        data::Data date_ex;
-
-        std::string DD = datacopy.substr(0, 2);
-        std::string MM = datacopy.substr(3, 2);
-        std::string YY = datacopy.substr(6, 4);
-
-        int D = atoi(DD.c_str());
-        int M = atoi(MM.c_str());
-        int Y = atoi(YY.c_str());
-
-        date_ex = { D, M, Y };
+        data::Data date_ex(datacopy);
 
         tm current_date = {};
 
-        current_date.tm_year = Y - 1900;
-        current_date.tm_mon = M - 1;
-        current_date.tm_mday = D;
+        current_date.tm_year = date_ex.year - 1900;
+        current_date.tm_mon = date_ex.month - 1;
+        current_date.tm_mday = date_ex.day;
 
         int num = 0;
         std::cout << "Type in 1 to print all of the Day." << std::endl;
@@ -136,25 +201,11 @@ namespace expenses
         }
         case 2:
         {
-            std::cout << "Categories for " << std::endl;
-            for (int i = 0; i < 7; ++i)
+
+            std::vector<Expenses*> all_expenses_by_week = Report_Expenses_Week(ListofExpenses);
+            for (auto expenses : all_expenses_by_week)
             {
-                std::time_t next_day = std::mktime(&current_date) + i * 24 * 60 * 60;
-                std::tm* next_date = std::localtime(&next_day);
-
-                std::cout << next_date->tm_mday << "." << next_date->tm_mon + 1 << "." << next_date->tm_year + 1900 << std::endl;
-
-                date_ex.day = next_date->tm_mday;
-                date_ex.month = next_date->tm_mon + 1;
-                date_ex.year = next_date->tm_year + 1900;
-
-                for (int a = 0; a < ListofExpenses.size(); ++a)
-                {
-                    if (date_ex.day == ListofExpenses[a].getDate().day && date_ex.month == ListofExpenses[a].getDate().month && date_ex.year == ListofExpenses[a].getDate().year)
-                    {
-                        std::cout << "Name: " << ListofExpenses[a].getName() << " Amount: " << ListofExpenses[a].getAmount() << std::endl;
-                    }
-                }
+                std::cout << "Name: " << expenses->getName() << " Amount: " << expenses->getAmount() << std::endl;
             }
             break;
         }
